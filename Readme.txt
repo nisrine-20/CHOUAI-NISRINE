e6e6a615284f4f55e8b7d09a2068efbcedd6ce6d
Nisrine <nisrinechouai83@gmail.com>
2024-05-25 17:43:36 +0100

    first comit

---
+++ Readme.txt
@@ -0,0 +1,21 @@
+How to run the online Birth Certificate System Project
+
+1.Extract the file and copy obcs folder
+
+2.Paste inside root directory(for xampp xampp/htdocs, for wamp wamp/www, for lamp var/www/html)
+
+3.Open PHPMyAdmin (http://localhost/phpmyadmin)
+
+4.Create a database with name obcsdb
+
+6.Import obcsdb.sql file(given inside the zip package in SQL file folder)
+
+7.Run the script http://localhost/obcs
+
+Admin Credential
+Username: admin
+Password: Test@123
+
+User Credential
+Username: 3636251425
+Password: Test@123
---
+++ SQL File/obcsdb.sql
@@ -0,0 +1,154 @@
+-- phpMyAdmin SQL Dump
+-- version 4.9.0.1
+-- https://www.phpmyadmin.net/
+--
+-- Host: 127.0.0.1
+-- Generation Time: May 17, 2024 at 07:13 AM
+-- Server version: 10.3.15-MariaDB
+-- PHP Version: 7.2.19
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+SET AUTOCOMMIT = 0;
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Database: `obcsdb`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `tbladmin`
+--
+
+CREATE TABLE `tbladmin` (
+  `ID` int(10) NOT NULL,
+  `AdminName` varchar(200) DEFAULT NULL,
+  `UserName` varchar(120) DEFAULT NULL,
+  `MobileNumber` bigint(10) DEFAULT NULL,
+  `Email` varchar(120) DEFAULT NULL,
+  `Password` varchar(200) DEFAULT NULL,
+  `AdminRegdate` timestamp NULL DEFAULT current_timestamp()
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Dumping data for table `tbladmin`
+--
+
+-- Updating the date to the current date (example date: '2024-05-17 19:05:09')
+INSERT INTO `tbladmin` (`ID`, `AdminName`, `UserName`, `MobileNumber`, `Email`, `Password`, `AdminRegdate`) VALUES
+(1, 'Admin', 'admin', 600000000, 'adminuser@example.com', 'f925916e2754e5e03f75dd58a5733251', CURRENT_TIMESTAMP);
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `tblapplication`
+--
+
+CREATE TABLE `tblapplication` (
+  `ID` int(10) NOT NULL,
+  `UserID` int(5) NOT NULL,
+  `ApplicationID` varchar(200) DEFAULT NULL,
+  `DateofBirth` varchar(200) DEFAULT NULL,
+  `Gender` varchar(50) DEFAULT NULL,
+  `FullName` varchar(200) DEFAULT NULL,
+  `PlaceofBirth` varchar(200) DEFAULT NULL,
+  `NameofFather` varchar(200) DEFAULT NULL,
+  `NameOfMother` varchar(120) DEFAULT NULL,
+  `PermanentAdd` mediumtext DEFAULT NULL,
+  `PostalAdd` mediumtext DEFAULT NULL,
+  `MobileNumber` bigint(10) DEFAULT NULL,
+  `Email` varchar(200) DEFAULT NULL,
+  `Dateofapply` timestamp NULL DEFAULT current_timestamp(),
+  `Remark` varchar(200) DEFAULT NULL,
+  `Status` varchar(50) DEFAULT NULL,
+  `UpdationDate` timestamp NULL DEFAULT NULL ON UPDATE current_timestamp()
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Dumping data for table `tblapplication`
+--
+
+-- Keeping only Fatima Zahra's information
+INSERT INTO `tblapplication` (`ID`, `UserID`, `ApplicationID`, `DateofBirth`, `Gender`, `FullName`, `PlaceofBirth`, `NameofFather`, `NameOfMother`, `PermanentAdd`, `PostalAdd`, `MobileNumber`, `Email`, `Dateofapply`, `Remark`, `Status`, `UpdationDate`) VALUES
+(1, 1, '794994267', '1995-03-15', 'Female', 'Fatima Zahra', 'Casablanca', 'Ahmed Benjelloun', 'Amina Benjelloun', '123 Rue de Marrakech, Casablanca', '123 Rue de Marrakech, Casablanca', 650000001, 'fatima.zahra@example.com', CURRENT_TIMESTAMP, 'Application approved.', 'Verified', CURRENT_TIMESTAMP);
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `tbluser`
+--
+
+CREATE TABLE `tbluser` (
+  `ID` int(10) NOT NULL,
+  `FirstName` varchar(200) DEFAULT NULL,
+  `LastName` varchar(200) DEFAULT NULL,
+  `MobileNumber` bigint(10) DEFAULT NULL,
+  `Address` mediumtext DEFAULT NULL,
+  `Password` varchar(200) NOT NULL,
+  `RegDate` timestamp NULL DEFAULT current_timestamp()
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Dumping data for table `tbluser`
+--
+
+-- Keeping only Fatima Zahra's user entry
+INSERT INTO `tbluser` (`ID`, `FirstName`, `LastName`, `MobileNumber`, `Address`, `Password`, `RegDate`) VALUES
+(1, 'Fatima', 'Zahra', 650000001, '123 Rue de Marrakech, Casablanca', 'f925916e2754e5e03f75dd58a5733251', CURRENT_TIMESTAMP);
+
+--
+-- Indexes for dumped tables
+--
+
+--
+-- Indexes for table `tbladmin`
+--
+ALTER TABLE `tbladmin`
+  ADD PRIMARY KEY (`ID`);
+
+--
+-- Indexes for table `tblapplication`
+--
+ALTER TABLE `tblapplication`
+  ADD PRIMARY KEY (`ID`);
+
+--
+-- Indexes for table `tbluser`
+--
+ALTER TABLE `tbluser`
+  ADD PRIMARY KEY (`ID`);
+
+--
+-- AUTO_INCREMENT for dumped tables
+--
+
+--
+-- AUTO_INCREMENT for table `tbladmin`
+--
+ALTER TABLE `tbladmin`
+  MODIFY `ID` int(10) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
+
+--
+-- AUTO_INCREMENT for table `tblapplication`
+--
+ALTER TABLE `tblapplication`
+  MODIFY `ID` int(10) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
+
+--
+-- AUTO_INCREMENT for table `tbluser`
+--
+ALTER TABLE `tbluser`
+  MODIFY `ID` int(10) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
+
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
